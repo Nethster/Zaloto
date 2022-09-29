8bcb629d4a71d3777697f62b4a44caae8204041d
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-09-29 14:02:22 +0200

    new nav

--- themes/zaloto/single.php
+++ themes/zaloto/single.php
@@ -3,18 +3,25 @@ get_header();
 ?>
 
 
-<div class="single-page-content">
-<p><?php the_time(get_option('date_format'));?></p>
-<h1><?php the_title(); ?></h1>
-<?php the_post_thumbnail('medium'); ?>
+<div class="single-page-wrapper">
 
+    <div class="single-page-content">
+        <p><?php the_time(get_option('date_format')); ?></p>
+        <h1><?php the_title(); ?></h1>
+
+        <div class="single-page-img">
+            <?php the_post_thumbnail('large'); ?>
+        </div>
+
+    </div>
+
+    <div class="single-page-link">
+
+        <?php the_content(); ?>
+        <a class="page-link" href="http://localhost/zaloto/news/"> Back</a>
+    </div>
 </div>
 
-<div class="single-page-text">
-
-<?php the_content(); ?>
-<a class="page-link" href="http://localhost/zaloto/news/"> Back</a>
-</div>
 
 
 
