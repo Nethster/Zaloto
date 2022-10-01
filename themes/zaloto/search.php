cebb85a780293ce73c1b756e090a487f4bbc1c8f
Tom Eriksson <58982536+Nethster@users.noreply.github.com>
2022-10-01 10:35:57 +0200

    just centered search result and added thumbnail

--- themes/zaloto/search.php
+++ themes/zaloto/search.php
@@ -3,25 +3,31 @@ get_header();
 
 
 ?>
+<div class="search-content-container">
 
-<div class="search-field">
-    <?php get_search_form(); ?>
-</div>
-
-
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <div class="search-result">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <p><?php the_excerpt(); ?></p>
-        </div>
-    <?php endwhile; ?>
-<?php else : ?>
-    <div class="search-result">
-        <h2><?php _e('Sorry, no posts matched your criteria.'); ?></h2>
+    <div class="search-field">
+        <?php get_search_form(); ?>
     </div>
-<?php endif; ?>
 
+
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="search-result">
+                <h2>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('thumbnail') ?>
+                        <?php the_title(); ?></a>
+                </h2>
+                <p><?php the_excerpt(); ?></p>
+            </div>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <div class="search-result">
+            <h2><?php _e('Sorry, no posts matched your criteria.'); ?></h2>
+        </div>
+    <?php endif; ?>
+
+</div>
 
 
 
