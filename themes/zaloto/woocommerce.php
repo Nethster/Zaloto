cc1f2d1e5514e3d073a55695fb955b1d273c2723
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-10-05 16:35:08 +0200

    Merge branch 'main' of https://github.com/Nethster/Zaloto

--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -2,24 +2,21 @@
 get_header();
 ?>
 
-
-<?php if (is_product_category()) { ?>
-    <div class="other_categories_header">
-        <?php $current_category_object = get_queried_object(); ?>
-        <h1><?php echo  $current_category_object->name; ?></h1>
-        <p><?php echo  $current_category_object->description; ?></p>
+<?php if (is_product_category()){?>
+     <div class="other_categories_page_header">
+     <?php $current_category_object = get_queried_object();?>
+     <h1><?php echo $current_category_object->name;?></h1>
+     <p> <?php echo  $current_category_object->description;?> </p>
     </div>
-<?php
-} ?>
-
-<div class="woocommerce">
     <?php
-    woocommerce_content();
+}
+    
     ?>
 </div>
 
-<?php
+<?php woocommerce_content(); ?>
 
+<?php
 if (is_product_category()) {
     get_template_part("/template-parts/blocks/categories");
 }
