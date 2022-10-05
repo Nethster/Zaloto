8b47cf4baa6464ad99c72b78bcb498a6bf28f80a
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-10-05 15:54:50 +0200

    done categories

--- themes/zaloto/template-parts/blocks/categories.php
+++ themes/zaloto/template-parts/blocks/categories.php
@@ -1,9 +1,9 @@
 <?php
 $current_category_object = get_queried_object();?>
 
-<div class="other_categories">
+<div class="other_categories_page">
 
-<div class="other-categories-text">
+<div class="other_categories_page_text">
 <h1><?php the_field('title', $current_category_object); ?></h1>
 <p><?php the_field('text', $current_category_object); ?></p>
 </div>
@@ -17,7 +17,7 @@ if( $terms ): ?>
      <?php  $thumbnail_id = get_term_meta($term->term_id, 'thumbnail_id', true);
             $image = wp_get_attachment_url($thumbnail_id); ?>
             <?php echo "<img src='{$image}' alt='' width='200' height='260' />"; ?>
-           <div class="categories_content"> 
+           <div class="categories_page_content"> 
             <h2><?php echo esc_html( $term->name ); ?></h2>
             <p><?php echo esc_html( $term->description ); ?></p>
             <a href="<?php echo esc_url( get_term_link( $term ) ); ?>">VIEW COLLECTION</a>
--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -2,20 +2,20 @@
 get_header();
 ?>
 
-
 <?php if (is_product_category()){?>
-    <div class="other_categories_header">
-  <?php  $current_category_object = get_queried_object();?>
-   <h1><?php echo  $current_category_object->name;?></h1>
-   <p><?php echo  $current_category_object->description;?></p>
-   </div>
-<?php
-}?>
+     <div class="other_categories_page_header">
+     <?php $current_category_object = get_queried_object();?>
+     <h1><?php echo $current_category_object->name;?></h1>
+     <p> <?php echo  $current_category_object->description;?> </p>
+    </div>
+    <?php
+}
+    
+    ?>
+
+<?php woocommerce_content(); ?>
 
 <?php
-
-woocommerce_content();
-
 if (is_product_category()) {
     get_template_part("/template-parts/blocks/categories");
 }
