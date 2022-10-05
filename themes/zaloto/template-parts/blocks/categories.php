a18d0149b256ce16a39687896fb16e7e9f327efd
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-10-05 09:12:48 +0200

    categories css

--- themes/zaloto/template-parts/blocks/categories.php
+++ themes/zaloto/template-parts/blocks/categories.php
@@ -1,23 +1,29 @@
-
 <?php
+$current_category_object = get_queried_object();?>
 
+<div class="other_categories">
 
-$terms = get_term('categories');
+<div class="heading-block intro-block">
+<h1><?php the_field('title', $current_category_object); ?></h1>
+<p><?php the_field('text', $current_category_object); ?></p>
+</div>
+<?php
+$terms = get_field('categories', $current_category_object);
+
 if( $terms ): ?>
     <ul>
     <?php foreach( $terms as $term ): ?>
         <li>
-            <h2><?php echo esc_html( $term->name ); ?></h2>
-            <p><?php echo esc_html( $term->description ); ?></p>
      <?php  $thumbnail_id = get_term_meta($term->term_id, 'thumbnail_id', true);
             $image = wp_get_attachment_url($thumbnail_id); ?>
-            echo "<img src='{$image}' alt='' width='260' height='365' />";
-            <a href="<?php echo esc_url( get_term_link( $term ) ); ?>">View collection '<?php echo esc_html( $term->name ); ?>' posts</a>
+            <?php echo "<img src='{$image}' alt='' width='180' height='260' />"; ?>
+           <div class="categories_content"> 
+            <h2><?php echo esc_html( $term->name ); ?></h2>
+            <p><?php echo esc_html( $term->description ); ?></p>
+            <a href="<?php echo esc_url( get_term_link( $term ) ); ?>">VIEW COLLECTION</a>
+            </div>
         </li>
     <?php endforeach; ?>
     </ul>
 <?php endif; ?>
-
-<?php
-var_dump($term);
-
+</div>
--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -2,12 +2,24 @@
 get_header();
 ?>
 
+
+<div class="categories-header">
+<?php if (is_product_category()){
+    $current_category_object = get_queried_object();
+   echo  $current_category_object->name;
+   echo  $current_category_object->description;
+
+}?>
+</div>
 <?php
- woocommerce_content(); 
+
+woocommerce_content(); 
 
 if (is_product_category()){
     get_template_part("/template-parts/blocks/categories");
 }
+
+
 ?>
 
 
