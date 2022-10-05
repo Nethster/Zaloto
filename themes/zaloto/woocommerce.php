d750226ebb98e0d7b8fc4175ad2ae9047fd2cd55
Tom Eriksson <58982536+Nethster@users.noreply.github.com>
2022-10-05 15:17:50 +0200

    hiding woocommerce h1

--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -3,18 +3,22 @@ get_header();
 ?>
 
 
-<?php if (is_product_category()){?>
+<?php if (is_product_category()) { ?>
     <div class="other_categories_header">
-  <?php  $current_category_object = get_queried_object();?>
-   <h1><?php echo  $current_category_object->name;?></h1>
-   <p><?php echo  $current_category_object->description;?></p>
-   </div>
+        <?php $current_category_object = get_queried_object(); ?>
+        <h1><?php echo  $current_category_object->name; ?></h1>
+        <p><?php echo  $current_category_object->description; ?></p>
+    </div>
 <?php
-}?>
+} ?>
+
+<div class="woocommerce">
+    <?php
+    woocommerce_content();
+    ?>
+</div>
 
 <?php
-
-woocommerce_content();
 
 if (is_product_category()) {
     get_template_part("/template-parts/blocks/categories");
