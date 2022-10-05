93cb0fbdcdf9121b674b1084622208d7c12ad800
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-10-05 11:08:39 +0200

    test merge with changes again

--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -2,8 +2,28 @@
 get_header();
 ?>
 
+<div class="other_categories_header">
+<?php if (is_product_category()){
+    $current_category_object = get_queried_object();?>
+   <h1><?php echo  $current_category_object->name;?></h1>
+   <p><?php echo  $current_category_object->description;?></p>
+<?php
+}?>
+</div>
+<?php
 
-<?php woocommerce_content(); ?>
+woocommerce_content(); 
+
+if (is_product_category()){
+    get_template_part("/template-parts/blocks/categories");
+}
+
+
+?>
+
+
+
+</div>
 
 
 <?php
