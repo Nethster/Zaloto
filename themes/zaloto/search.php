f164aeb674af027f2d82a6cc14d560d2c09f329b
Tom Eriksson <58982536+Nethster@users.noreply.github.com>
2022-10-05 20:29:08 +0200

    mer search styling

--- themes/zaloto/search.php
+++ themes/zaloto/search.php
@@ -5,10 +5,11 @@ get_header();
 ?>
 
 
-
-<div class="search-title"> <?php the_title(); ?> </div>
+<div class="search-page-title"> <?php the_title(); ?> </div>
 <h2 class="search-heading">SEARCH</h2>
-<p class="looking">What are you looking for? </p>
+<div class="search-text">
+    <p>What are you looking for? </p>
+</div>
 
 <div class="search-container">
     <div class="search-form">
