0f246bfa0f6498a206cee8c3124675adff6466cc
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-10-05 11:43:01 +0200

    header fix

--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -2,18 +2,16 @@
 get_header();
 ?>
 
-<<<<<<< HEAD
 
 <?php if (is_product_category()){?>
     <div class="other_categories_header">
   <?php  $current_category_object = get_queried_object();?>
    <h1><?php echo  $current_category_object->name;?></h1>
    <p><?php echo  $current_category_object->description;?></p>
+   </div>
 <?php
 }?>
 
-
-</div>
 <?php
 
 woocommerce_content();
@@ -21,13 +19,7 @@ woocommerce_content();
 if (is_product_category()) {
     get_template_part("/template-parts/blocks/categories");
 }
-
-
 ?>
-
-
-
-</div>
 
 
 <?php
