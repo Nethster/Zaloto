cf917952b9ec8f43ca2e1dc6d4d8342bea725981
Lorena Echeverri <lorena.echeverri@medieinstitutet.se>
2022-10-05 11:38:07 +0200

    is product categories

--- themes/zaloto/woocommerce.php
+++ themes/zaloto/woocommerce.php
@@ -2,9 +2,10 @@
 get_header();
 ?>
 
-<div class="other_categories_header">
-<?php if (is_product_category()){
-    $current_category_object = get_queried_object();?>
+
+<?php if (is_product_category()){?>
+    <div class="other_categories_header">
+  <?php  $current_category_object = get_queried_object();?>
    <h1><?php echo  $current_category_object->name;?></h1>
    <p><?php echo  $current_category_object->description;?></p>
 <?php
